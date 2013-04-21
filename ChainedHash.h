204af1eda2cee3a9412db79d9b525410954cbf94
JoePeacock <jpeacockinc@gmail.com>
2013-04-21 12:12:13 -0400

    Fixed copy error

--- ChainedHash.h
+++ ChainedHash.h
@@ -1,232 +1,122 @@
-#ifndef STOCK_H_
-#define STOCK_H_
+#ifndef MORPHED_VALLI_H_
+#define MORPHED_VALLI_H_
 
+#include "DList.h"
+#include <vector>
+using std::vector;
 
-
-#include <string>
-#include <cmath>
-using std::string;
-
-class Stock
+template <typename I>
+class MorphedValli
 {
 public:
-	const string name;
-	const size_t capital;
-	const double init_price;
-
-
-	Stock(const string& name, const size_t& capital, const size_t& init_price)
-		:name(name), capital(capital), init_price(init_price){init();}
-
-	Stock()
-		:name(""), capital(0), init_price(0.0){init();}
-
-
-	Stock(const string& name)
-		:name(name), capital(0), init_price(0.0){init();}
-
-	Stock(const char* name)
-		:name(name), capital(0), init_price(0.0){init();}
-
-
-	bool operator<(const Stock& rhs) const
+	class iterator
 	{
-		return name < rhs.name;
-	}
+		typename DList<I>::iterator list_iter;
 
-	bool operator==(const Stock& rhs) const
-	{
-		return name == rhs.name;
-	}
+		iterator(const typename DList<I>::iterator& l_iter)
+			:list_iter(l_iter){}
 
+		friend class MorphedValli<I>;
 
-	size_t Volume() const
-	{
-		return volume;
-	}
+	public:
+		iterator(const iterator& other)
+			:list_iter(other.list_iter){}
 
-	double PercentUp() const
-	{
-		return percent_up;
-	}
-
-	double PercentDown() const
-	{
-		return percent_down;
-	}
-
-	double PercentChange() const
-	{
-		return percent_change;
-	}
-
-	double Momentum() const
-	{
-		return momentum;
-	}
-
-	size_t Trend() const
-	{
-		return trend;
-	}
-
-
-	void process_trade(size_t shares, double price)
-	{
-		prev_price = curr_price;
-		curr_price = price;
-		volume += shares;
-		percent_up = (curr_price - init_price) / init_price * 100.0;
-		percent_down = -percent_up;
-		percent_change = fabs(percent_up);
-		momentum = percent_change * volume / capital;
-		if (trend_sign == +1 && curr_price >= prev_price ||
-			trend_sign == -1 && curr_price <= prev_price)
-			trend++;
-		else if (curr_price > prev_price)
+		iterator& operator=(const iterator& rhs)
 		{
-			trend = 1;
-			trend_sign = +1;
+			list_iter = rhs.list_iter;
+			return *this;
 		}
-		else if (curr_price < prev_price)
-		{
-			trend = 1;
-			trend_sign = -1;
-		}
-		else
-		{
-			trend = 0;
-			trend_sign = 0;
-		}
-	}
 
+		I& operator*() const
+		{
+			return *list_iter;
+		}
+
+		iterator& operator++()
+		{
+			++list_iter;
+			return *this;
+		}
+
+		iterator operator++(int)
+		{
+			iterator oldMe = *this;
+			list_iter++;
+			return oldMe;
+		}
+
+		bool operator==(const iterator& rhs) const
+		{
+			return list_iter == rhs.list_iter;
+		}
+
+		bool operator!=(const iterator& rhs) const
+		{
+			return list_iter != rhs.list_iter;
+		}
+	};
 
 private:
-	size_t volume;
-	double percent_up;
-	double percent_down;
-	double percent_change;
-	double momentum;
-	size_t trend;
-
-	double curr_price;
-	double prev_price;
-	int trend_sign;
-
-	void init()
-	{
-		volume = 0;
-		percent_up = 0.0;
-		percent_down = 0.0;
-		percent_change = 0.0;
-		momentum = 0.0;
-		trend = 0;
-
-		curr_price = init_price;
-		prev_price = init_price;
-		trend_sign = 0;
-	}
-};
-
-
-
-class Proxy_byVolume
-{
-	const Stock* ptr;
+	DList<I> list;
+	vector<iterator> vec;
+	size_t n, buckets;
 
 public:
-	Proxy_byVolume(const Stock* pointer)
-		:ptr(pointer){}
-
-	Proxy_byVolume()
-		:ptr(NULL){}
-
-	Proxy_byVolume(const Proxy_byVolume& rhs)
-		:ptr(rhs.ptr){}
-
-	Proxy_byVolume& operator=(const Proxy_byVolume& rhs)
+	explicit MorphedValli(size_t buckets = 10000)
+		:list(DList<I>())
+		,vec(vector<iterator>())
+		,n(0), buckets(buckets)
 	{
-		ptr = rhs.ptr;
-		return *this;
+		for (size_t bucket = 0; bucket < buckets; bucket++)
+		{
+			iterator iter = iterator(list.insert(list.end(), I()));
+			vec.push_back(iter);
+		}
+		vec.push_back(iterator(list.end()));
 	}
 
-	const Stock& operator*() const
+	~MorphedValli(){}
+
+	iterator begin() const
 	{
-		return *ptr;
+		return iterator(list.begin());
 	}
 
-	bool operator<(const Proxy_byVolume& rhs) const
+	iterator end() const
 	{
-		return ptr->Volume() < rhs.ptr->Volume();
+		return iterator(list.end());
 	}
 
-	bool operator==(const Proxy_byVolume& rhs) const
+	iterator find(const I& item) const
 	{
-		return ptr->Volume() == rhs.ptr->Volume();
+		// To do ...
+	}
+
+	iterator insert(const I& item)
+	{
+		size_t h = hash(item) % buckets;
+		for (iterator iter = vec[h]; iter != vec[h+1]; iter++)
+			if (*iter == item)
+				return iter;
+		++n;
+		return iterator(list.insert(vec[h+1].list_iter, item));
+	}
+
+	size_t size() const
+	{
+		return n;
+	}
+
+	bool empty() const
+	{
+		return size() == 0;
+	}
+
+	string str() const
+	{
+		return list.toString();
 	}
 };
-
-
-
-class Proxy_byPercentUp
-{
-	 // To do ...
-};
-
-
-
-class Proxy_byPercentDown
-{
-	 // To do ...
-};
-
-
-
-class Proxy_byPercentChange
-{
-	 // To do ...
-};
-
-
-
-class Proxy_byMomentum
-{
-	 // To do ...
-};
-
-
-
-class Proxy_byTrend
-{
-	 // To do ...
-};
-
-
-
-template<class T>
-inline bool operator<=(const T& lhs, const T& rhs)
-{
-	return lhs < rhs || lhs == rhs;
-}
-
-template<class T>
-inline bool operator>(const T& lhs, const T& rhs)
-{
-	return !(lhs <= rhs);
-}
-
-template<class T>
-inline bool operator>=(const T& lhs, const T& rhs)
-{
-	return lhs > rhs || lhs == rhs;
-}
-
-template<class T>
-inline bool operator!=(const T& lhs, const T& rhs)
-{
-	return !(lhs == rhs);
-}
-
-
 
 #endif
