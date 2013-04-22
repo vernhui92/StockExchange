9c5b062c7c7b4dd886a571a5c0f170760e31f46e
JoePeacock <jpeacockinc@gmail.com>
2013-04-22 13:51:14 -0400

    testing

--- HeapJAP.h
+++ HeapJAP.h
@@ -5,6 +5,17 @@
 #ifndef StockExchange_HeapJAP_h
 #define StockExchange_HeapJAP_h
 
+template<class I, class comp= std::lessthan()>
+class Heap {
+
+	Vector<int> arr;
+	for (int i = 0; i < 10; i ++) {
+		arr.push_back(i);
+	}
+
+
+};
+
 
 
 #endif
