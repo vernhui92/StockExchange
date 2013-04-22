81595a5a9ecbc724603241c48a0c49ca54925a3e
Joe Peacock <jpeacockinc@gmail.com>
2013-04-21 21:28:05 -0700

    Client is now complete save for inserting into hash and then working in the heap functions. Core add, whipser and pause are complete.

--- StockClientJAP.cpp
+++ StockClientJAP.cpp
@@ -8,60 +8,65 @@
 #include "StockJAP.h"
 using namespace std;
 
-Stock StockInputBuffer(const string &input) {
+void StockInput(const string &input) {
 	vector<string> split; // Create vector to hold our words
 	string buf; // Have a buffer string
 	stringstream ss(input); // Insert the string into a stream 
 
-	int actionIn = input.find('?');
-	if (actionIn > 0) {
-		string action = input.substr(0, actionIn);
-		if (action == "add") {
-			while (ss >> buf) {
-				split.push_back(buf);
-			}
-			int size = split.size()-1;
-			int priceStart = split[size].find('s'); // get location of 's' in last pos of vec.
-			size_t priceEnd = split[size].size(); // get the size of the last element in the vec
-			split.push_back(split[size].substr(priceStart+1, priceEnd)); // get the price and append
-			split[size] = split[size].substr(0, priceStart); // replace the second last with volume
+	while (ss >> buf) {
+		split.push_back(buf);
+	}
+	int size = split.size()-1;
+	int priceStart = split[size].find('s'); // get location of 's' in last pos of vec.
+	size_t priceEnd = split[size].size(); // get the size of the last element in the vec
+	split.push_back(split[size].substr(priceStart+1, priceEnd)); // get the price and append
+	split[size] = split[size].substr(0, priceStart); // replace the second last with volume
 
-			double initPrice = atof(split[3].c_str());
+	double initPrice = atof(split[3].c_str());
 
-			Stock StockItem = new Stock(split[1], atoi(split[2].c_str()), initPrice);
+	Stock StockItem(split[1], atoi(split[2].c_str()), initPrice);
 
-			// Slap this Stock Item into the HashTable at position based on StockItem.name hash
+	// Slap this Stock Item into the HashTable at position based on StockItem.name hash
 
-			string StockName = StockItem.name;
+	string StockName = StockItem.name;
 
-			// This gives us a Hashtable full of StockItem Obects.
+	// This gives us a Hashtable full of StockItem Obects.
 
-			StockItem.process_trade(1400, 14.75);
-			StockItem.process_trade(1400, 14.75);
+	cout << StockName << " starts at " << StockItem.init_price << endl;
+	cout << StockItem.PercentUp() << "% change" << endl;
 
-			cout << StockName << " starts at " << StockItem.init_price << endl;
-			cout << StockItem.PercentUp() << "% change" << endl;
+	// TODO: Insert into HashTable here.. 
+};
 
-			// TODO: Insert into HashTable here.. 
+void ProcessStockTrade(const string &input) {
+	vector<string> split; // Create vector to hold our words
+	string buf; // Have a buffer string
+	stringstream ss(input); // Insert the string into a stream 
 
-			return StockItem;
-
-		}
-		else if (action == "whisper") {
-			cout << input.substr(actionIn+2, input.size()) << endl;
-		}
-		else if (action == "pause") {
-			cout << "Pausing..." << endl;
-		}
-	} 
-	else {
-		while (ss >> buf) {
-			split.push_back(buf);
-		} 
-		cout <<  split[0] <<  " " << split[1] << endl;
+	while (ss >> buf) {
+		split.push_back(buf);
 	}
 
-};
+	int size = split.size()-1;
+	int priceStart = split[size].find('s'); // get location of 's' in last pos of vec.
+	size_t priceEnd = split[size].size(); // get the size of the last element in the vec
+	split.push_back(split[size].substr(priceStart+1, priceEnd)); // get the price and append
+	split[size] = split[size].substr(0, priceStart); // replace the second last with volume
+
+	string StockName = split[0];
+	int Shares = atoi(split[1].c_str());
+	double Price = atof(split[2].c_str());
+
+	// Run the Hash function on StockName, find the item in the Hashtable and then do:
+	// Stock.process_trade(Shares, Price);
+	// Done
+
+	cout << StockName << " traded at " << Price << " and " << Shares << " shares." << endl;
+
+
+}	
+
+
 
 int main(int argc, char* argv[]) {
 	if (argc < 2) { 
@@ -82,9 +87,16 @@ int main(int argc, char* argv[]) {
 				if (actionIn > 0) {
 					string action = line.substr(0, actionIn);
 					if (action == "add") {
-						cout << &StockInputBuffer(line) << endl;
-/*						vec.push_back(&StockInputBuffer(line));						
-*/					}
+						StockInput(line);				
+					}
+					else if (action == "whisper") {
+						cout << line.substr(actionIn+2, line.size()) << endl;
+					}
+					else if (action == "pause") {
+						cout << "Pausing..." << endl;
+					}
+				} else {
+					ProcessStockTrade(line);
 				}
 
 			}	
